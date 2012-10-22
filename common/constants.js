c826df6fc0d7bdf71d8665e503d29cb71e02d1e4
Andre Karlsson <andre.karlsson@brandairport.se>
2012-10-22 10:00:38 +0200

    Added D3 graph and did some refactoring.

---
+++ common/constants.js
@@ -0,0 +1,5 @@
+var HOUR = 3600;
+var DAY = HOUR * 24;
+var WEEK = DAY * 7;
+var MONTH = DAY * 30;
+var YEAR = DAY * 365;
--- tellstick.js
+++ tellstick.js
@@ -1,29 +1,141 @@
 Sensors = new Meteor.Collection("sensors");
 SensorReadings = new Meteor.Collection("sensor_readings");
 SensorStats = new Meteor.Collection("sensor_stats");
-TL = TLog.getLogger(TLog.LOGLEVEL_MAX,true);
 
 if (Meteor.is_client) {
   Meteor.subscribe("sensors");
   var handle;
-  
-  Template.sensor_list.sensors = function(){
+
+  /**
+   * Debug output for handlebar contexts, log to console.
+   *
+   * Usage: {{debug}} or {{debug optionalValue}}
+   * 
+   */
+  Handlebars.registerHelper("debug", function(optionalValue) { 
+    console.log("Current Context"); 
+    console.log("--------------------"); 
+    console.log(this);   
+    if (optionalValue) { 
+      console.log("Value"); 
+      console.log("--------------------"); 
+      console.log(optionalValue); 
+    } 
+  });
+
+  /**
+   * Iterate over an object, setting 'key' and 'value' for each property in
+   * the object.
+   *
+   * Usage: {{#key_value obj}} Key: {{key}} // Value: {{value}} {{/key_value}}
+   *
+   * author: strathmyer, https://gist.github.com/1371586
+   *  
+   */
+  Handlebars.registerHelper("key_value", function(obj, fn) {
+      var buffer = "",
+          key;
+
+      for (key in obj) {
+          if (obj.hasOwnProperty(key)) {
+              buffer += fn({key: key, value: obj[key]});
+          }
+      }
+
+      return buffer;
+  });
+
+  Template.sensorList.sensors = function(){
     return Sensors.find({});
   }
 
-  Template.sensor_info.sensor_info = function(id, options){
-    console.log(SensorStats.findOne({}));
-    return options.fn(SensorStats.findOne({}));
-  }
+  Template.sensorInfo.helpers({
+      sensorInfo: function(id, options){
+        //console.log(SensorStats.findOne({}));
+        return options.fn(SensorStats.findOne({}));
+      },
+      sensorPlot: function(data){
+        if(data === ''){
+          return false;
+        }
 
-  Template.sensor_list.events = { 
+        var margin = {top: 20, right: 20, bottom: 30, left: 50},
+        width = 480 - margin.left - margin.right,
+        height = 200 - margin.top - margin.bottom;
+
+        var parseDate = d3.time.format("%d-%b-%y").parse;
+
+        var x = d3.time.scale()
+            .range([0, width]);
+
+        var y = d3.scale.linear()
+            .range([height, 0]);
+
+        var xAxis = d3.svg.axis()
+            .scale(x)
+            .orient("bottom");
+
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient("left");
+
+        var line = d3.svg.line()
+            .x(function(d) { return x(d.timestamp); })
+            .y(function(d) { return y(d.value); });
+
+            d3.select("svg").remove();
+
+        if(d3.select("svg") !== null){
+          var svg = d3.select("body").append("svg")
+                    .attr("width", width + margin.left + margin.right)
+                    .attr("height", height + margin.top + margin.bottom)
+                    .append("g")
+                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        }else{
+          var svg = d3.select("svg");
+        }
+            
+        
+        data.forEach(function(d) {
+          d.timestamp = new Date(d.timestamp*1000);
+          d.value = +d.value;
+        });
+
+
+
+        x.domain(d3.extent(data, function(d) { return d.timestamp; }));
+        y.domain(d3.extent(data, function(d) { return d.value; }));
+
+        svg.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxis);
+
+        svg.append("g")
+          .attr("class", "y axis")
+          .call(yAxis)
+          .append("text")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 6)
+          .attr("dy", ".71em")
+          .style("text-anchor", "end");
+
+        svg.append("path")
+          .datum(data)
+          .attr("class", "line")
+          .attr("d", line);
+        }
+
+  });
+
+  Template.sensorList.events = { 
     'click a': function(event){
         if(handle !== undefined){
           handle.stop();
         }
 
         handle = Meteor.subscribe("sensor_stats", event.target.id, function onComplete(){
-          console.log(SensorStats.find({}).count());
+          console.log("subscription complete");
         })
 
         return false;
@@ -32,60 +144,6 @@ if (Meteor.is_client) {
 
 }
 
-if (Meteor.is_server) {
 
-  Meteor.publish("sensors", function () {
-    var self = this;
-    var handle = Sensors.find({}).observe({
-      added: function (doc, idx) {
-        self.set("sensors", doc._id, doc);
-        self.flush();
-      },
-      removed: function (doc, idx) {
-        self.unset("sensors", doc._id, _.keys(doc));
-        self.flush();
-      },   
-      changed: function (newDocument, atIndex, oldDocument) {
-        //Only update on name change
-        if(newDocument.name !== oldDocument.name){
-          self.set("sensors", doc._id, doc);
-          self.flush();
-        }
-      }
-    });
-
-    // remove data and turn off observe when client unsubs
-    self.onStop(function () {
-      handle.stop();
-    });
-
-  });
-
-  Meteor.publish("sensor_stats", function (id) {
-    var self = this;
-    var uuid = Meteor.uuid();
-    var handle = SensorReadings.find({id: id}).observe({
-      added: function (doc, idx) {
-        statObj = {
-          id: id,
-          currentData: SensorReadings.findOne({id: id}).data
-        }
-        self.set("sensor_stats", uuid, statObj);
-        self.flush();
-      },
-      removed: function (doc, idx) {
-        self.unset("sensor_stats", doc._id, _.keys(doc));
-        self.flush();
-      }
-    });
-
-    self.complete(); 
-
-    // remove data and turn off observe when client unsubs
-    self.onStop(function () {
-      handle.stop();
-    });
-
-    });
-}
+  
 
